6d90acb15070527a3683aab8c13d24da67dbb57f
GytisKlnt <gytis.kalanta@gmail.com>
2022-06-08 13:16:48 +0300

    init

---
+++ src/components/Button/Button.styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background: transparent;
+  border-radius: 0.5rem;
+  border: 0.05 solid black;
+  cursor: pointer;
+  font-family: "Roboto", sans-serif;
+  padding: 0.5rem 1rem;
+`;
